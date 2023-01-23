70adafd3dd8cc4f5870d0e0481444003e9008fc7
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-01-23 18:32:31 -0500

    Copy - Cut - Paste

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -2,7 +2,7 @@
 word = "Holberton"
 word_last_2 = "Hol"
 word_first_3 = "on"
-middle_word = word_last_2 [1:3] + word [3:-2] + word_first_3 [0]
+middle_word = word_last_2[1:3] + word[3:-2] + word_first_3[0]
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
