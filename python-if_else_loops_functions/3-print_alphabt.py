57cae380e7e33350d2888b46607ca4a398fcd446
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-01-24 17:49:58 -0500

    print alphabet no q and e

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -2,7 +2,7 @@
 len = 97
 letter = ""
 while len < 123:
-    if chr(len).isalpha() and chr(len) not in ['q', 'e']:
+    if chr(len) and chr(len) not in ['q', 'e']:
         letter = letter + chr(len)
     len = len + 1
 print("{:s}".format(letter))
