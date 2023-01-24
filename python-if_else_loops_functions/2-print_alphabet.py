d5e8ca86fc7ccc318bb1a3ba2c6dfe3cc699ffad
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-01-24 16:58:47 -0500

    print alphabet

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-letter = 97
-while letter < 123:
-    if chr(letter):
-        print(chr(letter), end='')
-    letter += 1
+len = 97
+letter = ""
+while len < 123:
+    if chr(len) and chr(len) not in ['q', 'p']:
+        letter = letter + chr(len)
+    len = len + 1
+print(f"{letter}")
