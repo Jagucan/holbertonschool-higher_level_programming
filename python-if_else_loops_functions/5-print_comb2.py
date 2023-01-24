6fcf0635ac0d8e8e766f6d4dcbd047940bde8edd
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-01-24 18:59:27 -0500

    print 00, ..., 99

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-n = []
 for num in range(100):
     if num < 99:
-        n.append("{:02}".format(num))
-    if num == 99:
-        n.append(str(num))
-print(", ".join(n))
+        print("{:02}".format(num), end=", ")
+    else:
+        print("{:02}".format(num), end="")
