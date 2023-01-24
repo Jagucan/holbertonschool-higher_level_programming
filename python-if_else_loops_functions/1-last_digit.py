68c2b9cd8cd2c83e4ed778c95045d56df4ad11e2
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-01-24 17:18:33 -0500

    The last digit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -5,17 +5,26 @@ num = int(str(number)[-1])
 
 if number > 0:
     if num > 5:
-        print(f"Last digit of {number} is {num} and is greater than 5")
+        print("Last digit of {:d} is {:d} and is greater than 5"
+              .format(number, num))
     elif num < 6:
-        print(f"Last digit of {number} is {num} and is less than 6 and not 0")
+        print("Last digit of {:d} is {:d} and is less than 6 and not 0"
+              .format(number, num))
     else:
-        print(f"Last digit of {number} is {num} and is zero")
+        print("Last digit of {:d} is {:d} and is zero"
+              .format(number, num))
+
 elif number < 0:
     if num > 0 & num < 6:
-        print(f"Last digit of {number} is -{num} and is less than 6 and not 0")
+        print("Last digit of {:d} is -{:d} and is less than 6 and not 0"
+              .format(number, num))
     elif num < 0 & num > 5:
-        print(f"Last digit of {number} is -{num} and is greater than 5")
+        print("Last digit of {:d} is -{:d} and is greater than 5"
+              .format(number, num))
     else:
-        print(f"Last digit of {number} is {num} and is zero")
+        print("Last digit of {:d} is {:d} and is zero"
+              .format(number, num))
+
 else:
-    print(f"Last digit of {number} is {num} and is 0")
+    print("Last digit of {:d} is {:d} and is 0"
+          .format(number, num))
