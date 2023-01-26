568f46d6b56d781cd9465ffbcc2df2ec9d74a7d8
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-01-26 12:23:36 -0500

    import more functions

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -4,12 +4,13 @@ from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
 
-calc_add = add(a, b)
-calc_sub = sub(a, b)
-calc_mul = mul(a, b)
-calc_div = div(a, b)
+if __name__ == "__main__":
+	calc_add = add(a, b)
+	calc_sub = sub(a, b)
+	calc_mul = mul(a, b)
+	calc_div = div(a, b)
 
-print("{:d} + {:d} = {:d}".format(a ,b ,calc_add))
-print("{:d} - {:d} = {:d}".format(a ,b ,calc_sub))
-print("{:d} * {:d} = {:d}".format(a ,b ,calc_mul))
-print("{:d} / {:d} = {:d}".format(a ,b ,calc_div))
+	print("{:d} + {:d} = {:d}".format(a ,b ,calc_add))
+	print("{:d} - {:d} = {:d}".format(a ,b ,calc_sub))
+	print("{:d} * {:d} = {:d}".format(a ,b ,calc_mul))
+	print("{:d} / {:d} = {:d}".format(a ,b ,calc_div))
