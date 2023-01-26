d77d40fef84fbbf1127ca461ba614b270475d11e
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-01-26 12:08:47 -0500

    import more functions

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div 
+
+a = 10
+b = 5
+
+calc_add = add(a, b)
+calc_sub = sub(a, b)
+calc_mul = mul(a, b)
+calc_div = div(a, b)
+
+print("{:d} + {:d} = {:d}".format(a ,b ,calc_add))
+print("{:d} - {:d} = {:d}".format(a ,b ,calc_sub))
+print("{:d} * {:d} = {:d}".format(a ,b ,calc_mul))
+print("{:d} / {:d} = {:d}".format(a ,b ,calc_div))
