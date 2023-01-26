284736658c7f18135734d7e041495e2f5cab232e
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-01-26 12:26:00 -0500

    import more functions

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -5,12 +5,12 @@ a = 10
 b = 5
 
 if __name__ == "__main__":
-	calc_add = add(a, b)
-	calc_sub = sub(a, b)
-	calc_mul = mul(a, b)
-	calc_div = div(a, b)
+    calc_add = add(a, b)
+    calc_sub = sub(a, b)
+    calc_mul = mul(a, b)
+    calc_div = div(a, b)
 
-	print("{:d} + {:d} = {:d}".format(a ,b ,calc_add))
-	print("{:d} - {:d} = {:d}".format(a ,b ,calc_sub))
-	print("{:d} * {:d} = {:d}".format(a ,b ,calc_mul))
-	print("{:d} / {:d} = {:d}".format(a ,b ,calc_div))
+    print("{:d} + {:d} = {:d}".format(a ,b ,calc_add))
+    print("{:d} - {:d} = {:d}".format(a ,b ,calc_sub))
+    print("{:d} * {:d} = {:d}".format(a ,b ,calc_mul))
+    print("{:d} / {:d} = {:d}".format(a ,b ,calc_div))
