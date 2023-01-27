a1cf2aadf62ae0007fefe8efdc7b8fbbfaf71632
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-01-27 13:08:10 -0500

    script dynamic!

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,6 +2,7 @@
 import sys
 
 def argument():
+
     argc = len(sys.argv) - 1
     if argc == 0:
         print("{:d} arguments." .format(argc))
@@ -16,4 +17,5 @@ def argument():
             print("{:d}: {}" .format(num, sys.argv[num]))
 
 if __name__ == "__main__":
+
     argument()
