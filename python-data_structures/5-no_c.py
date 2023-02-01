04edac245c544282333763a4c3efd163ba5a5af1
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-01-31 21:27:46 -0500

    Replace in a copy

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for len in my_string:
-        if len not in ["c", "C"]:
-            print("{}".format(len), end="")
+    not_c = ""
+
+    if my_string is not None:
+        for len in my_string:
+            if len not in ["c", "C"]:
+                not_c += len
+
+        return("{}".format(not_c))
