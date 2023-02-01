bbfb6fbc2599627e3967df832c65c8e6cba14ccb
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-01-31 19:56:42 -0500

    print integer

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -4,4 +4,6 @@ def print_list_integer(my_list=[]):
     for num in my_list:
         if isinstance(num, int):
             print("{:d}".format(num))
-
+        elif isinstance(num, str):
+            print("Traceback (most recent call last):")
+            break
