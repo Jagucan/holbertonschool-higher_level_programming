2c56af38a33609e77d9a4bbc80a87068e0c2f885
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-02-01 12:51:57 -0500

    Matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     if matrix is not None:
         for row in matrix:
             for num in row:
-                if row is not None:
+                if row is not "\n":
                     if isinstance(num, int):
                         print("{:d}".format(num), end=" ")
 
