92e8aa7c0633bf6d70ede5107a2f05b2ccb64743
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-02-01 15:08:46 -0500

    Find the max

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+
+    max_int = my_list[0]
+    for item in my_list:
+        if isinstance(item, int) and item > max_int:
+            max_int = item
+
+    return max_int
