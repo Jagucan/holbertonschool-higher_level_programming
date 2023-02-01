df7f01ef7f84fa217f4400ae812002d5141480bf
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-01-31 21:16:08 -0500

    print reversed

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
 
-    for num in my_list[::-1]:
-        if isinstance(num, int):
-            print("{:d}".format(num))
+    if my_list != None:
+        for num in my_list[::-1]:
+            if isinstance(num, int):
+                print("{:d}".format(num))
 
-        elif isinstance(num, str):
-            print("Traceback (most recent call last):")
-            break
-        
+            elif isinstance(num, str):
+                print("Traceback (most recent call last):")
+                break
