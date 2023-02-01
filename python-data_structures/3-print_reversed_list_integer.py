a7d95fd053e38be75c27d52016b6b351663e4e0c
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-01-31 21:12:28 -0500

    print reversed

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
 
-    new_list = ""
-    for num in my_list:
-        new_list = str(num) + new_list
-    print(str.format(new_list))
+    for num in my_list[::-1]:
+        if isinstance(num, int):
+            print("{:d}".format(num))
 
-if __name__ == "__main__":
-    print_reversed_list_integer(my_list=[])
+        elif isinstance(num, str):
+            print("Traceback (most recent call last):")
+            break
+        
