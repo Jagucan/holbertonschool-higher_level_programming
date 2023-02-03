71b35e59585ed4fada1fd8380cfd8579d03e6aa8
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-02-03 14:46:39 -0500

    mult by 2

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     mult = \
-        dict(map(lambda item:(item[0], item[1] * 2), a_dictionary.items()))
+        dict(map(lambda item: (item[0], item[1] * 2), a_dictionary.items()))
     return mult
