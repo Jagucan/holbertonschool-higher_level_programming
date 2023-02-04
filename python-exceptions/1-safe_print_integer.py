df6d108b0d6ce71b9beee21c598d290a886c0685
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-02-03 19:20:34 -0500

    prints an integer

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    if value != int and value == "":
+    if value != int and value == None:
         return False
     try:
-        print("{:d}".format((value)))
-        return True
+        if isinstance(value, int):
+            print("{:d}".format((value)))
+            return True
     except ValueError:
         return False
