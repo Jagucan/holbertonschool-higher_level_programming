17a7038105e0ab4cbb1637751e14058383f4fd91
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-02-06 16:45:59 -0500

    Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """ This is the Square class. """
+
+    def __init__(self, size):
+        """ Initializes a new instance of the Square class """
+        self.__size = size
