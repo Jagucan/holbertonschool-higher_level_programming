32241de571fc1d4c25d32dfc5589ef97316ffb75
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-02-06 16:37:46 -0500

    My first square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """ My first square """
+
 class Square:
     """ This is the Square class. """
+
     def __init__(self):
         """ Initializes a new instance of the Square class """
+
         pass
