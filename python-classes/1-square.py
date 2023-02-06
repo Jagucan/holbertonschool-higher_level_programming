cc8c6f9c89261797cee5634efa7d95484eb60e5b
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-02-06 16:55:53 -0500

    Square with size

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" Module that defines the Square class.
 
+    This module provides a class called Square,
+    that can be used to create square objects.
+"""
 
 class Square:
     """ This is the Square class. """
