d3609ce551fd852365b334f9588964c755f499a3
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-02-06 17:30:11 -0500

    Size validation

---
+++ python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Module that defines the Square class.
+
+    This module provides a class called Square,
+    that can be used to create square objects.
+"""
+
+
+class Square:
+    """ This is the Square class. """
+
+    def __init__(self, size=0):
+        """ Initializes a new instance of the Square class """
+        self.__size = size
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
