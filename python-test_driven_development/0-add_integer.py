37d1282cade6c91adcdcf9e05c530dc038d510c9
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-02-08 15:47:18 -0500

    Integers addition

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,19 +3,20 @@
     returns the result of the sum.
 """
 
+
 def add_integer(a, b=98):
     """ adds two numbers and converts them to integers
-        before returning the result 
+        before returning the result
     """
 
     if not isinstance(a, (int, float)):
-        raise TypeError ("a must be an integer")
+        raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
     if isinstance(a, (int, float)):
         a = int(a)
-    if  isinstance(b, (int, float)):
+    if isinstance(b, (int, float)):
         b = int(b)
 
     return a + b
