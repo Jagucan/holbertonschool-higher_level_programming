19f3652a804ea1c9b64fa246c08df4ede17106a7
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-02-10 17:22:14 -0500

    Simple Rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Simple Rectangle """
+
+
+class Rectangle:
+    """ This is the Rectangle class. """
+
+    def __init__(self):
+        """ Initializes a new instance of the Rectangle class """
+
+        pass
