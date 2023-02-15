08231a6246b687b1905c5b7b266d8175018a96ed
Jagucan <jjgc.2499@gmail.com>
2023-02-14 19:21:10 -0500

    Readme

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -4,7 +4,13 @@
 """
 
 
-def lookup(obj):
-    """ Returns the list of available attributes and methods of an object. """
-    return [atributo for atributo in dir(obj)\
-        if not callable(getattr(obj, atributo)) and not atributo.startswith("__")]
+def lookup(objeto):
+    """ Returns the list of available attributes
+        and methods of an object
+    """
+    attributes = dir(object)
+    attribute_list = []
+    for attributes in attributes:
+        if not callable(getattr(object, attributes)) and not attributes.startswith("__"):
+            attribute_list.append(attributes)
+    return attribute_list
