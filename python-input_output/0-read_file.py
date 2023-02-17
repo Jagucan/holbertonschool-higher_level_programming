d0767c83c0197bda935344d7000922e8b72b8549
Jagucan <jjgc.2499@gmail.com>
2023-02-17 16:30:26 -0500

    Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-""" _summary_ """
+""" Reads a text file (UTF8) and prints
+    it to stdout using with.
+"""
 
 
 def read_file(filename=""):
+    """ Read an file """
+
     with open(filename, mode="r", encoding="UFT8") as my_file:
         content = my_file.read()
         print(content)
