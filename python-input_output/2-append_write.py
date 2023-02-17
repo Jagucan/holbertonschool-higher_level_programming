47a32a9afeff3f6b5e93f804fc0bd576517b8d6c
Jagucan <jjgc.2499@gmail.com>
2023-02-17 16:56:04 -0500

    Appends a string

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Appends a string at the end of a text file (UTF8)
+    and returns the number of characters added:
+"""
+
+
+def append_write(filename="", text=""):
+    """ Appends a string in a text file """
+
+    with open(filename, mode="w", encoding="UTF-8") as my_file:
+        my_file.seek(2)
+        num = my_file.write(text)
+        return num
