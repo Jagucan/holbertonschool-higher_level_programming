70fd0d72ce3ec3b8518e64ff9a9dab099c9deb8b
Jagucan <jjgc.2499@gmail.com>
2023-02-17 17:02:33 -0500

    Appends a string

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -7,7 +7,6 @@
 def append_write(filename="", text=""):
     """ Appends a string in a text file """
 
-    with open(filename, mode="w", encoding="UTF-8") as my_file:
-        my_file.seek(2, 0)
+    with open(filename, mode="a", encoding="UTF-8") as my_file:
         num = my_file.write(text)
         return num
