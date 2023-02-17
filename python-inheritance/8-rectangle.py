9e6c2b69f0d4383d00afe0d40e2b0688b113d761
Jagucan <jjgc.2499@gmail.com>
2023-02-17 09:17:59 -0500

    Rectangle

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" returns True or False if the object is exactly
+    an instance of the specified class.
+"""
+
+
+class BaseGeometry:
+    """_summary_
+    """
+
+    def area(self):
+        """_summary_
+        """
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """_summary_
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """_summary_
+    """
+
+    def __init__(self, width, height):
+        """ Initialize a new Rectangle instance """
+
+        self.__width = self.integer_validator("wigth", width)
+        self.__height = self.integer_validator("height", height)
