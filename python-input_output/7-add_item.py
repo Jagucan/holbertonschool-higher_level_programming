b9d1e4ad4697b25d963bd354538b2c3ffd3b5ba4
Jagucan <jjgc.2499@gmail.com>
2023-02-17 20:06:17 -0500

    commit

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
 """ Adds all arguments to a Python list, and then save them to a file """
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-""" Import save_to_json_file to 5-save_to_json_file"""
-
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-""" Import load_from_json_file to 6-load_from_json_file """
-
 import sys
 """ Import sys """
-
 from os import path
 """ Import PATH """
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+""" Import save_to_json_file to 5-save_to_json_file"""
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+""" Import load_from_json_file to 6-load_from_json_file """
 
 
 filename = "add_item.json"
 """ Python list """
-
 args = sys.argv[1:]
 """ Arguments """
-
 my_list = []
 if path.exists(filename):
     my_list = load_from_json_file(filename)
