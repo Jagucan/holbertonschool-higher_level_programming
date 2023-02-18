631b6f6357be1ee6916687e9ee575ecc25768a05
Jagucan <jjgc.2499@gmail.com>
2023-02-17 19:08:11 -0500

    To JSON string

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-""" """
+""" Returns the JSON representation of an object (string) """
 
 
 import json
+""" Import JSON Module"""
 
 def to_json_string(my_obj):
-    """ """
+    """ JSON - string """
     
     return json.dumps(my_obj)
