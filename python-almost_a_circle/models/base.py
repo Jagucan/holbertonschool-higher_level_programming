956537e02bb872c35edef26094e121f71067a70d
Jagucan <jjgc.2499@gmail.com>
2023-02-22 10:38:00 -0500

    Base class

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
+"""_summary_
+"""
+
+
 class Base():
+    """_summary_
+    """
     __nb_objects = 0
+
     def __init__(self, id=None):
+        """_summary_
+
+        Args:
+            id (_type_, optional): _description_. Defaults to None.
+        """
         if id is not None:
             self.id = id
         else:
