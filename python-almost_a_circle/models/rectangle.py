80cab4b35560453c857ac31318fefdfec6457f40
Jagucan <jjgc.2499@gmail.com>
2023-02-22 10:41:01 -0500

    First Rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
+"""
+    Returns:
+        _type_: _description_
+"""
 from models.base import Base
+"""_summary_
+
+    Returns:
+        _type_: _description_
+"""
 
 class Rectangle(Base):
+    """_summary_
+
+    Args:
+        Base (_type_): _description_
+    """
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.__width = width
@@ -11,32 +25,72 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """_summary_
+
+        Args:
+            value (_type_): _description_
+        """
         self.__width = value
 
     @property
     def height(self):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """_summary_
+
+        Args:
+            value (_type_): _description_
+        """
         self.__height = value
 
     @property
     def x(self):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
         return self.__x
 
     @x.setter
     def x(self, value):
+        """_summary_
+
+        Args:
+            value (_type_): _description_
+        """
         self.__x = value
 
     @property
     def y(self):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
         return self.__y
 
     @y.setter
     def y(self, value):
+        """_summary_
+
+        Args:
+            value (_type_): _description_
+        """
         self.__y = value
