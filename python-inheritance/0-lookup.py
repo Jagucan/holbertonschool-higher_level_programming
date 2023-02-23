3f539b6e5aac56ca6be04c4d14152b49302d390b
Jagucan <jjgc.2499@gmail.com>
2023-02-23 16:20:54 -0500

    commit

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
-""" Module that returns the list
-    of available attributes
+""" Module that returns the list of available attributes
 """
 
 
-def lookup(objeto):
-    """ Returns the list of available attributes
-        and methods of an object
-    """
-    attributes = dir(object)
-    attribute_list = []
-    for attributes in attributes:
-        if not callable(getattr(object, attributes)) and not attributes.startswith("__"):
-            attribute_list.append(attributes)
-    return attribute_list
+def lookup(obj):
+    """ returns a list of available attributes and methods of an object """
+    attributes = dir(obj)
+    attributes = [a for a in attributes if a.startswith('_')]
+    return attributes
