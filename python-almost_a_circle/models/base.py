65fa9fb2915c32bb62beccb854b349556d515564
Jagucan <jjgc.2499@gmail.com>
2023-02-24 18:11:29 -0500

    commit

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -3,8 +3,7 @@
 import json
 
 
-
-class Base():
+class Base:
     """ Base class """
     __nb_objects = 0
 
@@ -26,10 +25,44 @@ class Base():
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """ Save Object to file """
+        """ Writes the JSON string representation of list_objs """
         if list_objs is None:
             list_objs = []
         filename = cls.__name__ + ".json"
         with open(filename, "w") as file:
-            list = [obj.to_dictionary() for obj in list_objs]
-            file.write(cls.to_json_string(list))
+            dict_list = [obj.to_dictionary() for obj in list_objs]
+            file.write(cls.to_json_string(dict_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ Returns the list of the JSON string representation """
+        if json_string is None or json_string == "":
+            return []
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """ Create an instance with attributes from a dictionary """
+        if cls.__name__ == "Rectangle":
+            width = dictionary.get("width", 1)
+            height = dictionary.get("height", 1)
+            instance = cls(width, height)
+        elif cls.__name__ == "Square":
+            size = dictionary.get("size", 1)
+            instance = cls(size)
+        else:
+            instance = cls()
+        return instance
+
+    @classmethod
+    def load_from_file(cls):
+        """ Return a list of instances """
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, encoding="utf-8") as f:
+                json_string = f.read()
+                list_dict = cls.from_json_string(json_string)
+                return [cls.create(**d) for d in list_dict]
+        except FileNotFoundError:
+            return []
