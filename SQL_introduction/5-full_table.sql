fad38916dfdda9b1a3f0068f87aa8b43a2e864f2
Jagucan <jjgc.2499@gmail.com>
2023-03-15 16:11:30 -0500

    Full description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
 -- Full description
-SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, IS_NULLABLE, COLUMN_DEFAULT
-FROM information_schema.columns
-WHERE table_name = 'first_table' AND table_schema = 'hbtn_0c_0';
+SHOW CREATE TABLE first_table;
