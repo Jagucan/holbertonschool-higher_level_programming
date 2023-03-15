5ae1f188302f0665b4003625cf3a3664a5979406
Jagucan <jjgc.2499@gmail.com>
2023-03-15 17:13:50 -0500

    Full creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -7,25 +7,25 @@ CREATE TABLE IF NOT EXISTS second_table(
 );
 
 INSERT INTO  second_table (id, name, score) VALUES(
-    '1'
+    '1',
     'Jhon',
     '10'
 );
 
 INSERT INTO  second_table (id, name, score) VALUES(
-    '2'
+    '2',
     'Alex',
     '3'
 );
 
 INSERT INTO  second_table (id, name, score) VALUES(
-    '3'
+    '3',
     'Bob',
     '14'
 );
 
 INSERT INTO  second_table (id, name, score) VALUES(
-    '4'
+    '4',
     'George',
     '8'
 );
