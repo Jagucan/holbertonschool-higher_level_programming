a0e8fbd440be8bdc438e7241c30feebd38df07bd
Jagucan <jjgc.2499@gmail.com>
2023-03-15 10:50:19 -0500

    List databases

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,17 +1,3 @@
 -- List databases
 
-import mysql.connector
-
-cnx = mysql.connector.connect(user='root', password='root')
-
-cursor = cnx.cursor()
-
-query = "SELECT schema_name FROM information_schema.schemata"
-cursor.execute(query)
-
-databases = cursor.fetchall()
-for db in databases:
-    print(db[0])
-
-cursor.close()
-cnx.close()
+SHOW DATABASES
