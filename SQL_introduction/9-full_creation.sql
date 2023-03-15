8111051c93a23ba5717eaee8657fe57f9fa35ad4
Jagucan <jjgc.2499@gmail.com>
2023-03-15 18:00:43 -0500

    Full creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -7,24 +7,26 @@ CREATE TABLE IF NOT EXISTS second_table(
     PRIMARY KEY (id)
 );
 
-ALTER TABLE second_table MODIFY COLUMN id INT AUTO_INCREMENT;
-
 INSERT INTO  second_table (name, score) VALUES(
+    '1',
     'Jhon',
     '10'
 );
 
 INSERT INTO  second_table (name, score) VALUES(
+    '2',
     'Alex',
     '3'
 );
 
 INSERT INTO  second_table (name, score) VALUES(
+    '3',
     'Bob',
     '14'
 );
 
 INSERT INTO  second_table (name, score) VALUES(
+    '4',
     'George',
     '8'
 );
