230ffb57a0620c3de57a1a4ca610bf1bee5c0dc8
Jagucan <jjgc.2499@gmail.com>
2023-03-16 11:19:17 -0500

    Number by score

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
--- Number Score 
-SELECT score AS 'score', COUNT(*) AS 'number' FROM second_table GROUP BY score;
+-- Number by score
+SELECT score AS 'score', COUNT(*) AS 'number' FROM second_table GROUP BY score ORDER BY score DESC;
