3537d7373d2d8550ad2350db760673d59d088181
Jagucan <jjgc.2499@gmail.com>
2023-03-17 16:13:40 -0500

    Get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,25 +6,22 @@ Arguments:
    username - MySQL username
    password - MySQL password
    database - Database name
-Use: ./0-select_states.py 
 """
 
 import sys
 import MySQLdb
-    """ import modules """
 
 
 if __name__ == "__main__":
-    """ get arguments """
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root", db="hbtn_0e_usa", charset="utf8")
-cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+    conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root", db="hbtn_0e_usa", charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
