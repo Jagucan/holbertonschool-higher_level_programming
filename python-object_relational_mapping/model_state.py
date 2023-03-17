2301a23fea767767e303e977ecc1107d1ccab681
Jagucan <jjgc.2499@gmail.com>
2023-03-17 18:24:18 -0500

    SQL Injection...

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -9,9 +9,12 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+"""  """
 
 class state(Base):
     """ Class state """
+
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, unique=True, autoincrement=True, nullable=False)
+    id = Column(Integer, primary_key=True, unique=True,
+                    autoincrement=True, nullable=False)
     name =  Column(String(128), nullable=False)
