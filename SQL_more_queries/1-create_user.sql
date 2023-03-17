98aac353d6a759c6f3cd797ceda3a0f95ec8f2e3
Jagucan <jjgc.2499@gmail.com>
2023-03-17 11:18:31 -0500

    create root user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- Lists all privileges of the MySQL users
-
 CREATE USER IF NOT EXISTS 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%';
+FLUSH PRIVILEGES;
