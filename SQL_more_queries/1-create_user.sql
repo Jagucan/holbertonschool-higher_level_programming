8e367c51a802980bcfe6ae858932e72f35b5074f
Jagucan <jjgc.2499@gmail.com>
2023-03-17 11:17:51 -0500

    create root user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- Lists all privileges of the MySQL users
-USE hbtn_0d_tvshows;
+
 CREATE USER IF NOT EXISTS 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%';
