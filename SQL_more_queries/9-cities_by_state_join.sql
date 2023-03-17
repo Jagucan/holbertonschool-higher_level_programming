ff2e6ecc726ceb845ae0e5b8c8bd1e55854f5306
Jagucan <jjgc.2499@gmail.com>
2023-03-17 14:33:27 -0500

    Cities by States

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- lists all cities contained in the database - Cities by States 
-SELECT cities.id, states.name AS name, cities.name FROM cities JOIN states ON cities.state_id = states.id;
+SELECT cities.id, cities.name, states.name AS name FROM cities JOIN states ON cities.state_id = states.id;
