7f048c1bb4c22470e5997a097a4f0e81f57fa381
Jagucan <jjgc.2499@gmail.com>
2023-03-17 12:31:10 -0500

    Read user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -6,5 +6,5 @@ USE hbtn_0d_2;
 
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
-GRANT SELECT ON user_0d_2.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbnt_0d_2.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
