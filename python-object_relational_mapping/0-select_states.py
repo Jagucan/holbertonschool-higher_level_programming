a4b9038d5b086cf44dbef06e6cb13fc0c34efd92
Jagucan <jjgc.2499@gmail.com>
2023-03-17 15:40:16 -0500

    Get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-""" Get all states """
+"""
+This script lists all states 
+from the database hbtn_0e_0_usa.
+"""
 
 import MySQLdb
 import sys
-" Import Module "
 
 
 if __name__ == "__main__":
-    """ """
-
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
