5dd46da83b6089f52b17c62fb772f83882126a35
Jagucan <jjgc.2499@gmail.com>
2023-03-17 11:07:37 -0500

    list privileges

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- Lists Users
+USE hbtn_0d_tvshows;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""  """
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root", db="hbtn_0d_usa", charset="utf8")
+cur = conn.cursor()
+cur.execute("SELECT * FROM states ORDER BY id ASC") # HERE I have to know SQL to grab all states in my database
+query_rows = cur.fetchall()
+for row in query_rows:
+    print(row)
+cur.close()
+conn.close()
