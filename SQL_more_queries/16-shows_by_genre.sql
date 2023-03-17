c645983518fa726799a2cfb17e9055068ceee474
Jagucan <jjgc.2499@gmail.com>
2023-03-17 15:23:26 -0500

    List shows and genres

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,4 @@
 -- List shows and genres 
-USE hbtn_0d_tvshows;
 SELECT tv_shows.title, IFNULL (tv_genres.name, 'NULL') AS name FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
