39c35b1b5537ab7c3916be1dedf0c98605ac37e2
Jagucan <jjgc.2499@gmail.com>
2023-03-18 18:25:15 -0500

    Delete states

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -23,9 +23,8 @@ if __name__ == "__main__":
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    
+
     state = session.query(State).filter(State.name.like('%a%')).all()
     for objects in state:
         session.delete(objects)
     session.commit()
-        
