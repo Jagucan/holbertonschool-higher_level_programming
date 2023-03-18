783f714f59728e93177fcfa19cbc87ddbe542edb
Jagucan <jjgc.2499@gmail.com>
2023-03-17 19:25:05 -0500

    SQL Injection...

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -17,5 +17,5 @@ class State(Base):
 
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, unique=True,
-                    autoincrement=True, nullable=False)
+                autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
