ec15b3f6ae42c33cb57e1c9a2200025c2246aa7b
Jagucan <jjgc.2499@gmail.com>
2023-03-17 19:17:53 -0500

    SQL Injection...

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
+"""State class"""
 
-"""
-This file contains the class definition of a State
-and an instance Base = declarative_base():
-"""
-
-from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-""" Import Modules """
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
-""" creates a base class to declare data models. """
 
-class state(Base):
-""" Class state that inherits from Base """
+
+class State(Base):
+    """Class State"""
 
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, unique=True,
-                    autoincrement=True, nullable=False)
+    id = Column(Integer, autoincrement=True, primary_key=True,
+                nullable=False, unique=True)
     name = Column(String(128), nullable=False)
