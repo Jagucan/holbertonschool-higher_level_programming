1f522a2a143471516cb9116080338f0bdc70dbbb
Jagucan <jjgc.2499@gmail.com>
2023-03-17 20:57:49 -0500

    All states via SQLAlchemy

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -6,6 +6,8 @@ Lists all State objects from the database hbtn_0e_6_usa
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base, State
 
 
