e862aa21290a58cd524c3320c71c8eb834604917
Jagucan <jjgc.2499@gmail.com>
2023-03-17 19:34:06 -0500

    Cities by states

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+This script lists all cities
+from the database hbtn_0e_4_usa
+"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    matchName = sys.argv[4].split(';')[0].strip("'")
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                                passwd=password, db=database, charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM cities ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
