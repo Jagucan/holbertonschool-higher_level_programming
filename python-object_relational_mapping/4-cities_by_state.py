d02243facecbbf1f054f1a90e07ca6d2744afe1a
Jagucan <jjgc.2499@gmail.com>
2023-03-17 19:39:27 -0500

    Cities by states

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=username,
                                 passwd=password, db=database, charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id WHERE states.name = %s ORDER BY cities.id ASC", (state,))
+    cur.execute("SELECT * FROM cities ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
