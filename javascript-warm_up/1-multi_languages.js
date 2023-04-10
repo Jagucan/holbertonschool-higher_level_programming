14d38d0e20eec6fb9b7d0ed1a8ae6148dce57a54
Jagucan <jjgc.2499@gmail.com>
2023-04-10 18:50:21 -0500

    3 languages

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const first_line = 'C is fun';
-const secon_line = 'Python is cool';
-const third_line = 'JavaScript is amazing';
+const firstLine = 'C is fun';
+const seconLine = 'Python is cool';
+const thirdLine = 'JavaScript is amazing';
 
-console.log(first_line);
-console.log(secon_line);
-console.log(third_line);
+console.log(firstLine);
+console.log(seconLine);
+console.log(thirdLine);
