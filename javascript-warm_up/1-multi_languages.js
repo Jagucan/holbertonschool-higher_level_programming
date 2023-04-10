553ffd87c02e96a2b8289fea0b2db35e0a7db530
Jagucan <jjgc.2499@gmail.com>
2023-04-10 18:46:28 -0500

    3 languages

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+var first_line = 'C is fun';
+var secon_line = 'Python is cool';
+var third_line = 'JavaScript is amazing';
+
+console.log(first_line);
+console.log(secon_line);
+console.log(third_line);
