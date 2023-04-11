8c20c36db37e0895d7a3c5997644d4833600e610
Jagucan <jjgc.2499@gmail.com>
2023-04-11 09:47:11 -0500

    Loop to languages

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const allLines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let line = 0; line < allLines.length; line++) {
+  console.log(allLines[line]);
+}
