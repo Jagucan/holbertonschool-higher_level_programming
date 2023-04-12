6ca7a8243de0f81baeb0a2a131af0ebb4238ece4
Jagucan <jjgc.2499@gmail.com>
2023-04-12 16:41:19 -0500

    Factorial

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+function factorial(num) {
+  if (isNaN(num) || num === 0 || num === 1) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+}
+console.log(factorial(args[0]));
