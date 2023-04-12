f3fa74674222d7af8b62635dbee22bbc3701b781
Jagucan <jjgc.2499@gmail.com>
2023-04-12 17:27:31 -0500

    Second biggest!

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+function findSecondBiggest(args) {
+  const num = args.map(Number);
+  const sortedArgs = num.sort((a, b) => b - a);
+
+  if (sortedArgs.length < 2) {
+    console.log(0);
+  } else {
+    console.log(sortedArgs[1]);
+  }
+}
+
+findSecondBiggest(args);
