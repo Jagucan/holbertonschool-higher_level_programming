8a14e455576dcc5fbe4cb47e16dc3f91ad611e93
Jagucan <jjgc.2499@gmail.com>
2023-04-18 11:28:06 -0500

    Rectangle #2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,13 +1,15 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor (w, h) {
-    if (w <= 0 || h <= 0) {
-      return null;
+  constructor(w, h) {
+    if (!isNaN(w) || !isNaN(h)) {
+      if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
+      } else {
+        return null;
+      }
     }
-    this.width = w;
-    this.height = h;
   }
 }
-
 module.exports = Rectangle;
