9cc89cdb1203f4a24fe88110fe5e1fd237214c85
Jagucan <jjgc.2499@gmail.com>
2023-04-18 17:54:28 -0500

    Square #1

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const BaseSquare = require('./5-square');
+
+class Square extends BaseSquare {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    let row = c.repeat(this.width);
+    for (let i = 0; i < this.height; i++) {
+      console.log(row);
+    }
+  }
+}
+module.exports = Square;
