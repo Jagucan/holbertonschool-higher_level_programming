5cc124348167f42573b6fd14246ea8b78fd6ff82
Jagucan <jjgc.2499@gmail.com>
2023-04-18 12:01:30 -0500

    print rectangle

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (!isNaN(w) || !isNaN(h)) {
+      if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
+      } else {
+        return null;
+      }
+    }
+  }
+  print () {
+    for (let l = 0; l <= this.height; l++) {
+      let rectangle = '';
+      for (let w = 1; w <= this.width; w++) {
+        rectangle += 'X';
+      }
+      console.log(rectangle);
+    }
+  }
+}
+
+module.exports = Rectangle;
