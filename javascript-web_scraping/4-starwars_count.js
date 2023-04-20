a87aa0d4534435ab49db8e28441d003c5ab72060
Jagucan <jjgc.2499@gmail.com>
2023-04-20 16:05:01 -0500

    Star wars Wedge Antilles

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+const request = require('request');
+const apiUrl = 'https://swapi-api.hbtn.io/api/films/';
+const characterId = 18;
+
+request(apiUrl, function (error, response, body) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const films = JSON.parse(body).results;
+  const wedgeFilms = films.filter((film) => {
+    return film.characters.includes(
+      'https://swapi-api.hbtn.io/api/people/' + characterId + '/'
+    );
+  });
+
+  console.log(wedgeFilms.length);
+});
