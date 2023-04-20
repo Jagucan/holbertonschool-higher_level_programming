b645757a7e396120499deec4dd922590dc953074
Jagucan <jjgc.2499@gmail.com>
2023-04-20 17:29:43 -0500

    How many completed?

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+const request = require('request');
+const apiUrl = process.argv[2];
+
+const completedTasks = {};
+
+request(apiUrl, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  const data = JSON.parse(body);
+  for (const task of data) {
+    if (task.completed) {
+      const userId = task.userId;
+      if (!completedTasks[userId]) {
+        completedTasks[userId] = 0;
+      }
+      completedTasks[userId]++;
+    }
+  }
+  console.log(completedTasks);
+});
