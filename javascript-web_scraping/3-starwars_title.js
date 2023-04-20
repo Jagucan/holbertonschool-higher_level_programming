3d2064115bd9c0a01df8db2b3b71209b6c43dd5c
Jagucan <jjgc.2499@gmail.com>
2023-04-20 15:27:16 -0500

    movie title

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+const request = require('request');
+const id = process.argv[2];
+
+request.get(
+  'https://swapi-api.hbtn.io/api/films/' + id,
+  function (error, response, body) {
+    if (error) {
+      console.log(error);
+    }
+    if (response.statusCode !== 200) {
+      console.log(error);
+    }
+    const data = JSON.parse(body);
+    console.log(data.title);
+  }
+);
