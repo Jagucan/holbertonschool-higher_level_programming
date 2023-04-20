50cf42144781791d8c2354f7b6dc08e1405d50a3
Jagucan <jjgc.2499@gmail.com>
2023-04-20 15:05:00 -0500

    Status code

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const request = require('request');
+const myUrl = process.argv[2];
+
+request.get(myUrl, (error, response) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
+});
