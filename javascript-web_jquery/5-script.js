66539454ce51e668f814db94e3b83eb92ce22749
Jagucan <jjgc.2499@gmail.com>
2023-04-20 20:33:16 -0500

    List of elements

--- javascript-web_jquery/5-script.js
+++ javascript-web_jquery/5-script.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+$(document).ready(function () {
+  $('DIV#add_item').click(function () {
+    var newElementAdded = $('<li>Item</li>');
+    newElementAdded.appendTo('UL.my_list');
+  });
+});
