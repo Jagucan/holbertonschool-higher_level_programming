f29dd79bbd90ea3d360b8540a3e6f7d32436c941
Jagucan <jjgc.2499@gmail.com>
2023-04-21 13:20:40 -0500

    get all title

--- javascript-web_jquery/8-script.js
+++ javascript-web_jquery/8-script.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+$(document).ready(function () {
+  const url = 'https://swapi-api.hbtn.io/api/films/?format=json';
+  $.getJSON(url, function (data) {
+    for (const title of data.results) {
+      $('<li>').text(title.title).appendTo('UL#list_movies');
+    }
+  });
+});
