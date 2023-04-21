7a97762ccd0b448f276cfdf774b65a147ca19f02
Jagucan <jjgc.2499@gmail.com>
2023-04-21 14:01:12 -0500

    Say Hello!

--- javascript-web_jquery/9-script.js
+++ javascript-web_jquery/9-script.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+$(document).ready(function () {
+  const url = 'https://stefanbohacek.com/hellosalut/?lang=fr';
+  $.getJSON(url, function (data) {
+    $('<p>').text(data['hello']).appendTo('DIV#hello');
+  });
+});
