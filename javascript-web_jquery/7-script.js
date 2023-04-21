d44936768adfc97e487f972b547f6d949f309f79
Jagucan <jjgc.2499@gmail.com>
2023-04-21 13:05:10 -0500

    get name

--- javascript-web_jquery/7-script.js
+++ javascript-web_jquery/7-script.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+$(document).ready(function () {
+  const url = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
+  $.getJSON(url, function (data) {
+    const name = data.name;
+    $('<p>').text(name).appendTo('div#character');
+  });
+});
